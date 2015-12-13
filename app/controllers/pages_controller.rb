260ee982cb00ae5dbc3159dc5cb1ecb78c8270e6
Josue Martinez <josuedaniel831@yahoo.com>
2015-12-13 17:18:01 +0000

    start adding carousel to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     def home
         @basic_plan = Plan.find(1)
         @pro_plan = Plan.find(2)
-        
+        @fountain_plan = Plan.find(3)
     end
     
     def about
