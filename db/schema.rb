b3f2eebd0bdbe9e6e2bc3e794126fe4351448230
Josue Martinez <josuedaniel831@yahoo.com>
2015-12-17 01:52:16 +0000

    Update forms for stripe

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20151207154059) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
